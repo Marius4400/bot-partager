18c6943ec9dbc7e904b5bcc3b32a895f5d01e078
lucioaltizii <altiziilucio@gmail.com>
2022-01-10 15:49:58 +0100

    goole image

--- deploy-commands.js
+++ deploy-commands.js
@@ -12,15 +12,18 @@ const commands = [
     new SlashCommandBuilder().setName('server').setDescription('Renvoie les infos du serveur!'),
     new SlashCommandBuilder().setName('user').setDescription('Renvoie les infos de l\'utilisateur!'),
     new SlashCommandBuilder().setName('time').setDescription('Renvoie les infos sur l\'heure'),
-    new SlashCommandBuilder().setName('googleimg').setDescription('Renvoie une image de nft sur google image'),
     new SlashCommandBuilder().setName('youtube').setDescription('Renvoie une video youtube sur les nfts'),
     new SlashCommandBuilder().setName('meteo').setDescription('Renvoie les infos sur la meteo'),
     new SlashCommandBuilder().setName('google').setDescription('Resultat de votre recherche : ')
         .addStringOption(option =>
             option.setName('recherche')
                 .setDescription('Votre recherche')
-                .setRequired(true))
-        
+                .setRequired(true)),
+    new SlashCommandBuilder().setName('googleimg').setDescription('Renvoie une image de nft sur google image')  
+        .addStringOption(option =>
+            option.setName('recherche')
+                .setDescription('Votre recherche')
+                .setRequired(true)),
 ]
 
     .map(command => command.toJSON());
